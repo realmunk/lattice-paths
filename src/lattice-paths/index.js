41d66e6eac8044da082f1edbc7c1e2e21c8806cd
Kristian Saksvik Munkvold <Kristian.Saksvik-Munkvold@gjensidige.no>
2019-03-13 11:21:26 +0100

    Setup of starting point for step-1

--- src/lattice-paths/index.js
+++ src/lattice-paths/index.js
@@ -18,20 +18,20 @@ function factorial(num) {
 // 3. You end in the bottom right corner
 
 function calculatePaths(_gridSize) {
-  // TODO: Calculate the number of possible paths through the grid.
-  // PS: The factorial function is here for a reason.
-
-  return "?";
+  return (
+    factorial(_gridSize + _gridSize) /
+    (factorial(_gridSize) * factorial(_gridSize))
+  );
 }
 
 // generatePaths() : generates the possible lattice paths
 
 // 1. A given path through the matrice is represented as a string
 // 2. The path only consists of two characters, representing East with 'E' and South with 'S'
+// 3. An example of a valid string follows: 'EEEEEEEEEEEESSSSSSSSSSSS'
 
 function generatePaths(_gridSize) {
   // TODO: Generate all the possible paths through the grid
-  // PS: This format is important, as it is what is expected across the program.
   return ["EEEEEEEEEEEESSSSSSSSSSSS"];
 }
 
